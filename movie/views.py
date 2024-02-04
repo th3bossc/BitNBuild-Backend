1d8feff3783f8e2f956ccffcc04836ea3c31934d
ArshiyaHafis <arshiyapadiyath@gmail.com>
2024-02-05 00:54:46 +0530

    upcoming, latest

--- movie/views.py
+++ movie/views.py
@@ -42,3 +42,48 @@ class GenreSearchView(APIView):
         genres_to_search = [genre]
         top_movies = get_top_movies(api_key, genres_to_search)
         return Response(top_movies.get(genre, []))
+
+
+class LatestMoviesView(APIView):
+    def get(self, request):
+        url = os.environ.get('URL') + '/movie/upcoming'
+        api_key = os.environ.get('API_KEY')
+        params = {'api_key': api_key, 'language': 'en-US', 'page': 1}
+        
+        response = requests.get(url, params=params)
+        print(response.json())
+
+        if response.status_code == 200:
+            data = response.json().get('results', [])
+            serializer = MovieSerializer(data=data, many=True)
+            print(data)
+            if serializer.is_valid():
+                return Response(serializer.data)
+            else:
+                return Response({'error': 'Invalid data', 'validation_errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({'error': 'Failed to fetch latest movies from MovieDB API'}, status=response.status_code)
+        
+
+
+class PopularMoviesView(APIView):
+    def get(self, request):
+        url = os.environ.get('URL') + '/movie/popular'
+        api_key = os.environ.get('API_KEY')
+        params = {'api_key': api_key, 'language': 'en-US', 'page': 1}
+        
+        response = requests.get(url, params=params)
+
+        if response.status_code == 200:
+            data = response.json().get('results', [])
+            serializer = MovieSerializer(data=data, many=True)
+            
+            if serializer.is_valid():
+                return Response(serializer.data)
+            else:
+                return Response({'error': 'Invalid data', 'validation_errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({'error': 'Failed to fetch popular movies from MovieDB API'}, status=response.status_code)
+        
+
+
